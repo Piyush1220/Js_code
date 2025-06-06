0d040b2b1ba5649d73a73fe2f1322353e6e35f51
Piyush1220 <patilpiyus01@gmail.com>
2025-06-06 16:05:12 +0530

    Variable_JS

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountID=42351
+let accountEmail = "abc@gmail.com"
+var accountPass = "1245"
+accountCity = "Amalner"
+
+// accountID = 7314 not allowed const
+
+accountEmail= "pwr@gmail.com"
+accountPass="5342"
+accountCity="Pune"
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountPass,accountCity]);
