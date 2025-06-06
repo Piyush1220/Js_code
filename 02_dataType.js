d45ebc393f741ddd8cf7c901a42f3fb3a5231349
Piyush1220 <patilpiyus01@gmail.com>
2025-06-06 16:37:38 +0530

    data types commit

---
+++ 02_dataType.js
@@ -0,0 +1,19 @@
+//we are using node not brower to run js code 
+
+let name ="Piyush" //string
+let Num = 10 //Number 
+let isLoggedIn = false //boolean
+
+console.log([name,Num,isLoggedIn])
+
+//undefined it mean value not define like let age;
+
+//primitive datatype
+//number = 2 to power 53
+// string
+//boolean = true / false
+//null= standalone value
+//symbol = unique
+
+
+
