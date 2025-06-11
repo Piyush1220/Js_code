2c2d798e9de1b16464abfb721c7cf7f9b16a8b86
Piyush1220 <patilpiyus01@gmail.com>
2025-06-11 14:10:48 +0530

    Conversion Operation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+let Score = 66;
+//let Score ="33abc"
+
+//console.log(typeof Score);
+
+// let valueNo = Number(Score)
+// console.log(typeof valueNo);
+// console.log(valueNo);
+
+//conversion
+// "33" => 33
+//"33abc" => NaN
+// null => 0
+//true => 1; false => 0
+
+
+let isLoggedIn = 1
+
+//let booleanIsLogIn = Boolean(isLoggedIn);
+//console.log(booleanIsLogIn);
+
+//1 - true; 0 - false;
+// "" - false
+// "53" - true
+
+let no = 90
+let stringNo = String(no)
+console.log(stringNo);
+console.log(typeof stringNo);
