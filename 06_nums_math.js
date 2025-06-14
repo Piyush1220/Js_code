488b94092b0cbeda7a044c968f9ed70366289fe7
Piyush1220 <patilpiyus01@gmail.com>
2025-06-14 17:04:45 +0530

    commits 06

---
+++ 06_nums_math.js
@@ -0,0 +1,39 @@
+const score = 120
+//console.log(score);
+
+const balance  = new Number(123.77123);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2 ))
+// console.log(balance.toPrecision(3));
+
+let num = 12000530;
+
+// console.log(num.toLocaleString("en-IN"));
+
+
+// ************************** Math *****************************
+
+// console.log(Math);
+
+// conversion in positive
+// console.log(Math.abs(-4));
+//  round the values
+// console.log(Math.round(4.4));
+
+// ceil is choice top value it means next number
+// console.log(Math.ceil(7.2));
+
+//  floor this round the number into less
+// console.log(Math.floor(6.9));
+
+// console.log(Math.max(6,9,23,12));
+// console.log(Math.min(4,53,21,6));
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random() * 10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
